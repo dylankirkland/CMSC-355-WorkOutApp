1818c9543694ebc818c63ca099def0786903b01d
dylankirkland <70165776+dylankirkland@users.noreply.github.com>
2020-09-20 19:45:30 -0400

    Add files via upload
    I am working on an interface to put in data about the user, like age and weight, and desired weight, possible more.

--- WorkoutApp3/app/src/main/java/com/example/workoutapp3/MainActivity.java
+++ WorkoutApp3/app/src/main/java/com/example/workoutapp3/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.workoutapp3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button confirmBttn = (Button) findViewById(R.id.confirmBttn);
+        confirmBttn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editTextWeight = (EditText) findViewById(R.id.editTextWeight);
+                EditText ageEditText = (EditText) findViewById(R.id.ageEditText);
+
+            }
+        });
+
     }
 }
