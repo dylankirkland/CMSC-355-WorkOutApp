574827c429d04b07b87c825746703c80a16e9ba9
DESKTOP-UN52407\Austin <saehaana@vcu.edu>
2020-09-21 13:08:56 -0400

    inclass commit

--- CMSC355_Project/app/src/main/java/com/example/cmsc355_project/MainActivity.java
+++ CMSC355_Project/app/src/main/java/com/example/cmsc355_project/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+//ausawin was here, inclass commit
 public class MainActivity extends AppCompatActivity {
     private Button buttonNext; //variable for button
     @Override
