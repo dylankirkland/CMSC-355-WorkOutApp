0d64c46b7ffc9ef711264e0618fb7d6cfd2f98cb
victorialdp <53184730+victorialdp@users.noreply.github.com>
2020-10-09 20:45:33 -0400

    Update MainActivity.java

--- CMSC355_Project/app/src/main/java/com/example/cmsc355_project/MainActivity.java
+++ CMSC355_Project/app/src/main/java/com/example/cmsc355_project/MainActivity.java
@@ -7,8 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-//ausawin was here, inclass commit
-
 /**
  * App's Main Welcome Screen
  * -------------------------
