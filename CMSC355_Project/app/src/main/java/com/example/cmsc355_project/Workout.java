9cad367c9dcdb62b90c01bb782935a447a6424a4
Phillip Palanca <palancapg@mymail.vcu.edu>
2020-10-14 21:46:00 -0400

    Coded out some of the Workout Object mostly worked on the addExercise and removeExercise methods. Not completely coded fully out since another method will be called within them.

--- CMSC355_Project/app/src/main/java/com/example/cmsc355_project/Workout.java
+++ CMSC355_Project/app/src/main/java/com/example/cmsc355_project/Workout.java
@@ -1,6 +1,7 @@
 package com.example.cmsc355_project;
 
-import java.util.List;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 
 /**
  *
@@ -11,23 +12,48 @@ import java.util.List;
  */
 
 public class Workout{
-    List<Exercise> exercises;
-
+    ArrayList<Exercise> exercises;
 
     //Default Constructor
     public Workout(){
     }
 
     //Parameterized Constructor
-    public Workout(List<Exercise> exercises){
+    public Workout(ArrayList<Exercise> exercises){
         this.exercises = exercises;
     }
 
     //Method on Adding exercises to Workout and should update list and reprint the list
+    public void addExercise(Exercise newExercise){
+        this.exercises.add(newExercise);
+
+        //reprint
+    }
 
     //Method on Removing specific from list and should update list and reprint the list
+    public void removeExercise(Exercise unwantedExercise, Boolean confirmation){
+
+        if(exercises.contains(unwantedExercise) && confirmation){ //Checks to see if the exercise is in the workout and the user actually wants to remove it -Palancapg
+            this.exercises.remove(unwantedExercise);
+        }
+        else{
+            System.out.println("Exercise can not be removed at this time");
+        }
+
+        //reprint with print format
+
+    }
+
+    //Method will rearrange list
+    public void rearrange(Exercise movingExercise){
+
+    }
 
     //Printing the list out in a specific format
+    public String print(){
+        return null;
+    }
+
 
 
 
