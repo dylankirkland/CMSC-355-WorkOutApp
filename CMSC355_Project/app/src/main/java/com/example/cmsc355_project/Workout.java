118b53fa87fbd4d0291611fc731b75c22113415f
Phillip Palanca <palancapg@mymail.vcu.edu>
2020-10-21 22:20:08 -0400

    1)Worked on the workout class and "finished it" 2)Created new activity called "CreateWokrkoutActivity" which basically will be the screen where the user will create a new workout 3)Create new issue

--- CMSC355_Project/app/src/main/java/com/example/cmsc355_project/Workout.java
+++ CMSC355_Project/app/src/main/java/com/example/cmsc355_project/Workout.java
@@ -23,6 +23,14 @@ public class Workout{
        this.workoutList = workoutList;
     }
 
+    public void setWorkoutList(ArrayList<Exercise> workoutList) {
+        this.workoutList = workoutList;
+    }
+
+    public ArrayList<Exercise> getWorkoutList() {
+        return workoutList;
+    }
+
     //Method on Adding exercises to Workout and should update list and reprint the list
     public void addExercise(Exercise newExercise){
         this.workoutList.add(newExercise);
@@ -41,7 +49,7 @@ public class Workout{
            System.out.println("Exercise: " + unwantedExercise.name + " looking to be removed is not found");
         }
         else if(workoutList.contains(unwantedExercise) && !confirmation){
-           System.out.println("Removal Process Stopped");
+           System.out.println("Removal Process Halted");
         }
         else{
             System.out.println("Exercise and Confirmation can not be found :(");
@@ -53,9 +61,20 @@ public class Workout{
     }
 
     //Method will rearrange list if a user wants to
-    public void rearrange(Exercise movingExercise){
-
+    public void rearrange(Exercise movingExercise, int index){
+        if(workoutList.get(index) == null) { //see if the index is filled with another exercise
+            this.workoutList.add(index, movingExercise); // if so place it in the index and the arrayList will shift to the right
+        }
+        else{
+            addExercise(movingExercise); // if not then it will add the exercise to the bottom of workout
+        }
     }
+
+    //Clears Workout and starts off with a fresh arrayList
+    public void restartWorkout(){
+        workoutList.clear();
+    }
+
 
     //Printing the list out in a specific format
     public void printWorkout(){
