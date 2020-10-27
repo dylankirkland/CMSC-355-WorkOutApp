79380e4fd4b0c034d5bbdcc137ccfe3d79db0618
Phillip Palanca <palancapg@mymail.vcu.edu>
2020-10-26 21:46:56 -0400

    1) Added some new buttons and views on to the CreateWorkoutActivity 2) Added a tool bar

--- CMSC355_Project/app/src/main/java/com/example/cmsc355_project/CreateWorkoutActivity.java
+++ CMSC355_Project/app/src/main/java/com/example/cmsc355_project/CreateWorkoutActivity.java
@@ -1,6 +1,7 @@
 package com.example.cmsc355_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -8,7 +9,14 @@ public class CreateWorkoutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Toolbar toolbar;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_workout);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("Create New Workout");
+
     }
 }
