e9c22ba931827bf6421b47d0dfbd337961d1f418
Dylan Kirkland <kirklanddc@mymail.vcu.edu>
2020-12-09 19:31:23 -0500

    worked on profile

--- CMSC355_Project/app/src/main/java/com/example/cmsc355_project/Profile.java
+++ CMSC355_Project/app/src/main/java/com/example/cmsc355_project/Profile.java
@@ -1,10 +1,16 @@
 package com.example.cmsc355_project;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Profile extends AppCompatActivity {
+    private EditText editTextWeight = findViewById(R.id.editTextWeight);
+    private EditText ageEditText = findViewById(R.id.ageEditText);
+    private Button confirm = findViewById(R.id.confirmBttn);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
