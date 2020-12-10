100cc93cd0c11bf20a2b0d74f56c10fca27588d5
Victoria Desktop <lopezdelpinmv@vcu.edu>
2020-12-10 04:36:19 -0500

    Modifications to user profile screen and addition of home button for user profile

--- CMSC355_Project/app/src/main/java/com/example/cmsc355_project/Profile.java
+++ CMSC355_Project/app/src/main/java/com/example/cmsc355_project/Profile.java
@@ -1,6 +1,8 @@
 package com.example.cmsc355_project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -15,5 +17,13 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
+    }
+
+    //Return to home screen on button click
+    public void onClickHome(View view)
+    {
+        //intent object, parameters passed are context and class we want to open (context,class)
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent); //pass intent created in line above
     }
 }
